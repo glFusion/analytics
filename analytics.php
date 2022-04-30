3954022c81bda914eca9240046ac569449c3a5f8
Lee Garner <lee@leegarner.com>
2022-04-30 16:56:43 -0700

    Bump version to beta 0.1.0

--- analytics.php
+++ analytics.php
@@ -8,7 +8,7 @@
  * @author      Lee Garner <lee@leegarner.com>
  * @copyright   Copyright (c) 2022 Lee Garner <lee@leegarner.com>
  * @package     analytics
- * @version     v0.0.1
+ * @version     v0.1.0
  * @license     http://opensource.org/licenses/gpl-2.0.php
  *              GNU Public License v2 or later
  * @filesource
@@ -20,11 +20,11 @@ if (!defined ('GVERSION')) {
 
 global $_DB_table_prefix, $_TABLES;
 
-Analytics\Config::set('pi_version', '0.0.2');
+Analytics\Config::set('pi_version', '0.1.0');
 Analytics\Config::set('gl_version', '2.0.0');
 
-$_AN_table_prefix = $_DB_table_prefix . 'ua_';
+$_UA_table_prefix = $_DB_table_prefix . 'ua_';
 
-$_TABLES['ua_trackers'] = $_AN_table_prefix . 'trackers';
-$_TABLES['ua_sess_info'] = $_AN_table_prefix . 'sess_info';
+$_TABLES['ua_trackers'] = $_UA_table_prefix . 'trackers';
+$_TABLES['ua_sess_info'] = $_UA_table_prefix . 'sess_info';
 
